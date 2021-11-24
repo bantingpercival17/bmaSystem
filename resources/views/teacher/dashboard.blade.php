3720f7562fd6143856336c37ceff4a5a9250c601
bantingpercival17 <percivalbanting@gmail.com>
2021-11-24 16:39:12 +0800

    teacher-dashboard

--- resources/views/teacher/dashboard.blade.php
+++ resources/views/teacher/dashboard.blade.php
@@ -67,56 +67,37 @@
                                                 </div>
                                             @elseif($_subject_data->is_approved === 0)
                                                 <div class="row">
-                                                    <div class="col-md-12">
-                                                        <form action="/teacher/subjects/grade-submission" method="post">
-                                                            <label for="" class="text-muted">GRADE SUBMISSION</label>
-                                                            @csrf
-                                                            <input type="hidden" name="_subject"
-                                                                value="{{ Crypt::encrypt($_subject->id) }}">
-                                                            <input type="hidden" name="_period"
-                                                                value="{{ $_subject_data->period }}">
-                                                            <input type="hidden" name="_form"
-                                                                value="{{ $_subject_data->form }}">
-                                                            <div class="form-group row">
-                                                                <div class="col-md">
-                                                                    <label for="" class="form-control">FORM
-                                                                        {{ strtoupper($_subject_data->form) }}</label>
-                                                                </div>
-                                                                <div class="col-md">
-                                                                    <button type="submit"
-                                                                        class="btn btn-info btn-block">RE-SUBMIT </button>
-                                                                </div>
-
-                                                            </div>
-
-                                                        </form>
-                                                    </div>
                                                     <div class="col-md">
                                                         <div class="row">
                                                             <div class="col-md">
-                                                                <p>
-                                                                    <small>GRADE SUBMISSION STATUS:</small>
-                                                                    <span class="text-danger"> <b>DISAPPRROVED</b>
-                                                                    </span>
-                                                                    <br>
-                                                                    <small>DATE SUBMISSION :</small> <span
-                                                                        class="text-muted"><b>{{ $_subject_data->created_at->format('M-d-y') }}</b></span>
-                                                                </p>
-                                                            </div>
-                                                            <div class="col-md">
-                                                                <small>VERIFIED BY:</small>
-                                                                <span
-                                                                    class="text-success"><b>{{ strtoupper($_subject_data->approved_by) }}</b>
+                                                                <small class="text-muted">GRADING STATUS:</small>
+                                                                <br>
+                                                                <span class="text-danger"> <b>DISAPPRROVED</b>
                                                                 </span>
                                                                 <br>
-                                                                <small>VERIFIED DATE :</small> <span
+                                                                <small class="text-muted">DATE SUBMISSION :</small>
+                                                                <br>
+                                                                <span class="text-muted">
+                                                                    <b>{{ $_subject_data->created_at->format('M-d-y') }}</b>
+                                                                </span>
+
+                                                            </div>
+                                                            <div class="col-md">
+                                                                <small class="text-muted">VERIFIED BY:</small>
+                                                                <span class="text-success">
+                                                                    <b>{{ strtoupper($_subject_data->approved_by) }}</b>
+                                                                </span>
+                                                                <br>
+                                                                <small class="text-muted">VERIFIED DATE :</small>
+                                                                <br>
+                                                                <span
                                                                     class="text-muted"><b>{{ $_subject_data->updated_at->format('M-d-y') }}</b></span>
                                                                 <br>
 
                                                             </div>
                                                         </div>
                                                         <p>
-                                                            <small>COMMENT:</small>
+                                                            <small class="text-muted">COMMENT:</small>
                                                             <span class="text-warning"> {{ $_subject_data->comments }}
                                                             </span>
                                                         </p>
@@ -140,7 +121,7 @@
                                         @else
                                             <small class="text-muted">GRADING STATUS:</small>
                                             <span class="text-muted">
-                                                <b>EDITTING</b>
+                                                <b>ON GOING</b>
                                             </span>
                                         @endif
                                     </div>
@@ -255,7 +236,7 @@
                                             @else
                                                 <small class="text-muted">GRADING STATUS:</small>
                                                 <span class="text-muted">
-                                                    <b>EDITTING</b>
+                                                    <b>ON GOING</b>
                                                 </span>
                                             @endif
                                         @else
