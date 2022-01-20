32f1c49f34aa3ffe14957bbd60f5d700084aa841
percival banting <percivalbanting@gmail.com>
2022-01-20 16:43:49 +0800

    Updated Grade Testing Formula  [01-20-2022]

--- app/Models/StudentDetails.php
+++ app/Models/StudentDetails.php
@@ -134,12 +134,7 @@ class StudentDetails extends Model
     }
     public function final_grade($_data, $_period)
     {
-        $_lec_grade_midterm = $this->lec_grade([$_data, 'midterm']);
-        $_lab_grade_midterm = $this->lab_grade([$_data, 'midterm']);;
-        $_lec_grade_final = $this->lec_grade([$_data, 'finals']);
-        $_lab_grade_final = $this->lab_grade([$_data, 'finals']);
-        $_total_final_grade = $_lab_grade_final > 0 ? ($_lec_grade_final + $_lab_grade_final) * .5 : (($_lec_grade_final / .4) * .5);
-
+        
         $_final_grade = 0;
         $midtermGradeLecture = $this->lec_grade([$_data, 'midterm']);
         $midtermGradeLaboratory = $this->lab_grade([$_data, 'midterm']);;
@@ -163,17 +158,6 @@ class StudentDetails extends Model
             }
         }
         return $_final_grade;
-
-        return  $_period == 'midterm' ?
-            // MIDTERM FORUMLA
-            ($_lab_grade_midterm > 0 ?  // IF THE GRADE OF LABORATORY ID ENCODED
-                (($_lec_grade_midterm + $_lab_grade_midterm)) // FORMULA FOR ENCODED LABORATORY : ((Lecture + Laboratory) * 50%)
-                : ($_lec_grade_midterm / .4))
-            // FINALS FORMULA
-            : ($_lab_grade_final > 0 ?  // IF THE GRADE OF LABORATORY IS ENCODED IN FINALS
-
-                (($_lec_grade_midterm + $_lab_grade_midterm) * .5) + (($_lec_grade_final + $_lab_grade_final) * .5) // LABOARTORY FORMULA   // FORMULA FOR (MLEC+MLAB)*.5 + (FLEC+FLAB)*.5
-                : (($_lec_grade_midterm / .4) * .5) + (($_lec_grade_final / .4) * .5)); // LECTURE FORMULA
     }
     public function percentage_grade($_grade)
     {
