ec2fb7576b09308e37240bd72b841724caa75c53
percival banting <percivalbanting@gmail.com>
2022-03-30 14:28:56 +0800

    Adminstrator Portal - Route [03-30-2022]

--- routes/custom-route/administrator.php
+++ routes/custom-route/administrator.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\PaymongoApi;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/dashboard', [AdministratorController::class, 'index'])->name('admin.dashboard'); // Dashboard
-Route::prefix('administrator')->group(function () {
+Route::prefix('administrator')->middleware(['administrator'])->group(function () {
     Route::get('/', [AdministratorController::class, 'index'])->name('admin.dashboard');
     Route::get('/dashboard', [AdministratorController::class, 'index'])->name('admin.dashboard'); // Dashboard
     Route::get('/enrollment/enrolled-list', [AdministratorController::class, 'dashboard_enrolled_list_view'])->name('admin.course-enrolled');
