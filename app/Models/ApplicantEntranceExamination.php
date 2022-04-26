56e93138a59fab41515aa0d0bca136fa48172a78
percival banting <percivalbanting@gmail.com>
2022-04-26 08:32:05 +0800

    Applicant Portal - Applicant Entrance Examination [04-26-2022]

--- app/Models/ApplicantEntranceExamination.php
+++ app/Models/ApplicantEntranceExamination.php
@@ -16,7 +16,7 @@ class ApplicantEntranceExamination extends Model
     {
         //$_item =  $this->applicant->course_id == 3 ? 100 : 200;
         return  $this->hasMany(ApplicantExaminationAnswer::class, 'examination_id')
-            ->join('bma_portal.examination_question_choices as eqc', 'eqc.id', 'applicant_examination_answers.question_id')
+            ->join('bma_portal.examination_question_choices as eqc', 'eqc.id', 'bma_website.applicant_examination_answers.question_id')
             ->where('eqc.is_answer', true)->sum('eqc.is_answer as total');
         #$_percent = ($_score / $_item) * 100;
         #$_passing = $this->applicant->course_id == 3 ? 50 : 50;
