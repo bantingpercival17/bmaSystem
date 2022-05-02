faa486a1c34149219d54db6d3191c6f99e7a705a
percival banting <percivalbanting@gmail.com>
2022-05-02 15:29:18 +0800

    Applicant Portal - Examination Result [02-05-2022]

--- resources/views/pages/general-view/applicants/entrance-examination-status.blade.php
+++ resources/views/pages/general-view/applicants/entrance-examination-status.blade.php
@@ -101,7 +101,7 @@
                                 </div>
                                 <div class="col-md-4">
                                     @if ($_data->applicant_examination)
-                                    {{$_data->applicant_examination}}
+                                        {{ $_data->applicant_examination }}
                                         <small
                                             class="badge bg-info">{{ $_data->applicant_examination->updated_at->format('F d, Y') }}</small>
                                         @if ($_data->applicant_examination->is_finish == 1)
