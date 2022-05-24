e7abde2510ba5f296280addf4a9ba4efbe203c7e
percival banting <percivalbanting@gmail.com>
2022-05-24 09:20:39 +0800

    Applicant Module - Sent Notification for Online Briefing [05-24-2022]

--- routes/custom-route/extra/applicant-route.php
+++ routes/custom-route/extra/applicant-route.php
@@ -5,8 +5,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth'])->group(function () {
     Route::get('/applicants', [ApplicantController::class, 'applicant_view'])->name('applicant-lists');
-    Route::get('/applicants/pre-register',[ApplicantController::class,'pre_applicant_view'])->name('applicant.pre-register')
-;    Route::get('/applicant-profile/view', [ApplicantController::class, 'applicant_profile'])->name('applicant-profile');
+    Route::get('/applicants/pre-register', [ApplicantController::class, 'pre_applicant_view'])->name('applicant.pre-register');
+    Route::get('/applicant-profile/view', [ApplicantController::class, 'applicant_profile'])->name('applicant-profile');
     Route::get('/applicant-profile/document-notification', [ApplicantController::class, 'applicant_document_notification'])->name('document-notification'); // Send Email for Document Attachment
     Route::get('/applicant-profile/document-verification', [ApplicantController::class, 'applicant_document_review'])->name('document-verification');
     Route::get('/applicants/removed', [ApplicantController::class, 'applicant_removed'])->name('applicant-removed');
@@ -16,8 +16,9 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/applicants/payment-verification', [ApplicantController::class, 'applicant_payment_verification'])->name('applicant-payment-verification');
     Route::get('/applicants/payment-verified', [ApplicantController::class, 'applicant_payment_verified'])->name('applicant-payment-verified');
     Route::get('/applicants/examination-notification', [ApplicantController::class, 'entrance_examination_notification'])->name('applicant-entrance-examination-notification');
+    Route::get('/applicants/briefing-notification', [ApplicantController::class, 'briefing_notification'])->name('applicant.briefing-notification');
     /* Examination  */
     Route::get('/applicant/examination', [ApplicantController::class, 'applicant_entrance_examination'])->name('applicant-examination-status');
     Route::get('/applicant/examination-reset', [ApplicantController::class, 'applicant_examination_reset'])->name('applicant-examination-reset');
-    ROute::get('/applicant/examination-remove',[ApplicantController::class,'examination_remove'])->name('examination.remove');
+    ROute::get('/applicant/examination-remove', [ApplicantController::class, 'examination_remove'])->name('examination.remove');
 });
