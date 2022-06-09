212e801c2a52a4c03a5290b2838ddc07de31f6d0
Percival Banting <percivalbanting@Percivals-MacBook-Air.local>
2022-06-09 11:13:51 +0800

    Applicant Module - Medical Result [06-09-2022]

---
+++ app/Models/ApplicantMedicalResult.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ApplicantMedicalResult extends Model
+{
+    use HasFactory;
+}
