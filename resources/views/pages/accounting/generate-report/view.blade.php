00f6ea6afe6d4691d7efd6261a410b619946260a
Percival Banting <percivalbanting@Percivals-MacBook-Air.local>
2022-07-15 13:59:11 +0800

    Accounting Portal - Generate Report [07-15-2022]

--- resources/views/pages/accounting/generate-report/view.blade.php
+++ resources/views/pages/accounting/generate-report/view.blade.php
@@ -5,8 +5,7 @@ $_title = 'Generate Report';
 @section('page-title', $_title)
 @section('beardcrumb-content')
     <li class="breadcrumb-item active" aria-current="page">
-        <svg width="14" height="14" class="me-2" viewBox="0 0 22 22" fill="none"
-            xmlns="http://www.w3.org/2000/svg">
+        <svg width="14" height="14" class="me-2" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path
                 d="M8.15722 19.7714V16.7047C8.1572 15.9246 8.79312 15.2908 9.58101 15.2856H12.4671C13.2587 15.2856 13.9005 15.9209 13.9005 16.7047V16.7047V19.7809C13.9003 20.4432 14.4343 20.9845 15.103 21H17.0271C18.9451 21 20.5 19.4607 20.5 17.5618V17.5618V8.83784C20.4898 8.09083 20.1355 7.38935 19.538 6.93303L12.9577 1.6853C11.8049 0.771566 10.1662 0.771566 9.01342 1.6853L2.46203 6.94256C1.86226 7.39702 1.50739 8.09967 1.5 8.84736V17.5618C1.5 19.4607 3.05488 21 4.97291 21H6.89696C7.58235 21 8.13797 20.4499 8.13797 19.7714V19.7714"
                 stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
@@ -52,7 +51,8 @@ $_title = 'Generate Report';
                                             <div class="col-md">
                                                 <div class="form-group">
                                                     <label class="text-label">Year</label>
-                                                    <input class="form-control" type="date" name="collection_date" id="">
+                                                    <input class="form-control" type="date" name="collection_date"
+                                                        id="">
                                                     @error('collection_date')
                                                         <span class="mt-2 badge bg-danger">{{ $message }}</span>
                                                     @enderror
@@ -80,9 +80,12 @@ $_title = 'Generate Report';
                                                 <div class="form-group">
                                                     <label for="">Academic Year</label>
                                                     <select name="_academic" class="form-control">
-                                                        <option value="4">2nd Semester | 2021 - 2022</option>
-                                                        <option value="3">1st Semester | 2021 - 2022</option>
-                                                        <option value="2">2nd Semester | 2020 - 2021</option>
+                                                        <option value="{{ base64_encode(4) }}">2nd Semester | 2021 - 2022
+                                                        </option>
+                                                        <option value="{{ base64_encode(3) }}">1st Semester | 2021 - 2022
+                                                        </option>
+                                                        <option value="{{ base64_encode(2) }}">2nd Semester | 2020 - 2021
+                                                        </option>
 
                                                     </select>
                                                 </div>
