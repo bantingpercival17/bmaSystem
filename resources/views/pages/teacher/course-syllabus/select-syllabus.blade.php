92af8becd05f667d91e570702e8177e2ed8617ae
Percival Banting <percivalbanting@Percivals-MacBook-Air.local>
2022-07-27 09:31:59 +0800

    Teacher Portal - Course Syllabus [07-27-2022]

--- resources/views/pages/teacher/course-syllabus/select-syllabus.blade.php
+++ resources/views/pages/teacher/course-syllabus/select-syllabus.blade.php
@@ -27,7 +27,7 @@ $_title = $_subject->section->section_name . ' | ' . $_subject->curriculum_subje
         @if (count($_course_syllabus) > 0)
             <div class="row">
                 @foreach ($_course_syllabus as $item)
-                    <div class="col-md-4">
+                    <div class="col-md">
                         <div class="card">
                             <div class="card-body">
                                 <div class="float-end">
