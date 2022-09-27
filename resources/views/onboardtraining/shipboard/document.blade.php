de8daf7c6a249a1525518fa1cfffb4cc87af3e98
Percival Banting <percivalbanting@Percivals-MacBook-Air.local>
2022-09-27 15:07:10 +0800

    Shipboard Portal - Documents

--- resources/views/onboardtraining/shipboard/document.blade.php
+++ resources/views/onboardtraining/shipboard/document.blade.php
@@ -12,8 +12,7 @@ $_title = 'Shipboard Monitoring';
 @endsection
 @section('beardcrumb-content')
     <li class="breadcrumb-item active" aria-current="page">
-        <svg width="14" height="14" class="me-2" viewBox="0 0 22 22" fill="none"
-            xmlns="http://www.w3.org/2000/svg">
+        <svg width="14" height="14" class="me-2" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path
                 d="M8.15722 19.7714V16.7047C8.1572 15.9246 8.79312 15.2908 9.58101 15.2856H12.4671C13.2587 15.2856 13.9005 15.9209 13.9005 16.7047V16.7047V19.7809C13.9003 20.4432 14.4343 20.9845 15.103 21H17.0271C18.9451 21 20.5 19.4607 20.5 17.5618V17.5618V8.83784C20.4898 8.09083 20.1355 7.38935 19.538 6.93303L12.9577 1.6853C11.8049 0.771566 10.1662 0.771566 9.01342 1.6853L2.46203 6.94256C1.86226 7.39702 1.50739 8.09967 1.5 8.84736V17.5618C1.5 19.4607 3.05488 21 4.97291 21H6.89696C7.58235 21 8.13797 20.4499 8.13797 19.7714V19.7714"
                 stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
@@ -24,7 +23,7 @@ $_title = 'Shipboard Monitoring';
     <div class="card mb-2">
         <div class="row no-gutters">
             <div class="col-md col-lg-2">
-                <img src="{{ $_midshipman? $_midshipman->profile_pic($_midshipman->account): 'http://bma.edu.ph/img/student-picture/midship-man.jpg' }}"
+                <img src="{{ $_midshipman ? $_midshipman->profile_pic($_midshipman->account) : 'http://bma.edu.ph/img/student-picture/midship-man.jpg' }}"
                     class="card-img " alt="#">
             </div>
             <div class="col-md-8 col-lg-8">
@@ -63,7 +62,7 @@ $_title = 'Shipboard Monitoring';
                                 <a class="nav-link" data-bs-toggle="collapse" href="#home-{{ $_journal->id }}"
                                     role="button" aria-expanded="false" aria-controls="home">
                                     <p
-                                        class="{{ $_journal->is_approved == null? 'text-muted': ($_journal->is_approved == 1? 'text-primary': 'text-danger') }} h5">
+                                        class="{{ $_journal->is_approved == null ? 'text-muted' : ($_journal->is_approved == 1 ? 'text-primary' : 'text-danger') }} h5">
                                         <b>{{ strtoupper($_journal->journal_type) }}</b>
                                     </p>
                                     @if ($_journal->is_approved == 1)
@@ -89,7 +88,8 @@ $_title = 'Shipboard Monitoring';
                                     <div class="form-group" id="narative-{{ $_journal }}">
 
                                         @if ($_journal->remark != null)
-                                            <label for="" class="text-muted h6"><b><small>REMARKS</small></b></label>
+                                            <label for=""
+                                                class="text-muted h6"><b><small>REMARKS</small></b></label>
                                             <textarea class="form-control" id="" cols="30" rows="4" disabled>{{ $_journal->remark }}</textarea>
                                         @endif
                                         <label for="" class="text-muted h6"><b><small>DOCUMENTS</small></b></label>
@@ -99,7 +99,7 @@ $_title = 'Shipboard Monitoring';
                                                 @foreach (json_decode($_journal->file_links) as $links)
                                                     <a class="btn-form-document col" data-bs-toggle="modal"
                                                         data-bs-target=".document-view-modal"
-                                                        data-document-url="{{ $links }}">
+                                                        data-document-url="{{ str_replace(':1000', '', $links) }}">
                                                         @php
                                                             $myFile = pathinfo($links);
                                                             $_ext = $myFile['extension'];
