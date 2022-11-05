8c711ed6a72da2e58a8939b64b86f52342fdf1f4
Percival Banting <percivalbanting@Percivals-MacBook-Air.local>
2022-11-05 09:00:14 +0800

    Employee Qr Code

--- resources/views/widgets/report/employee/daily_report_attendance.blade.php
+++ resources/views/widgets/report/employee/daily_report_attendance.blade.php
@@ -1,29 +1,38 @@
-@extends('widgets.report.report_layout')
+@extends('widgets.report.app_report_template')
 @section('title-report', 'DAILY ATTENDANCE REPORT')
 @section('form-code', 'ACC-14 ')
 @section('content')
-    <div class="content-1">
+    <div class="content">
         <h3 class="text-center"><b>DAILY ATTENDANCE REPORT</b></h3>
         <p>Date:
             <b><u>{{ request()->input('_date') ? date_format(date_create(request()->input('_date')), 'M d,Y') : now()->format('M d,Y') }}</u></b>
         </p>
 
-        <table class="attendance-table">
+        <table class="table-2">
             <thead>
                 <tr>
-                    <th>DEPARTMENT</th>
+                    <th>QR CODE</th>
                     <th>NAME</th>
-                    <th width="100">TIME-IN</th>
-                    <th width="100">TIME-OUT</th>
+                    <th>DEPARTMENT</th>
+                    {{-- <th width="100">TIME-IN</th>
+                    <th width="100">TIME-OUT</th> --}}
 
                 </tr>
             </thead>
             <tbody>
                 @foreach ($_employees as $_employee)
                     <tr>
+                        <td class="text-center">
+                            <img src="data:image/png;base64, {!! base64_encode(
+                                QrCode::style('round', 0.5)->eye('square')->size(200)->generate($_employee->user->email),
+                            ) !!} "> <br>
+                            {{--  {{ $_employee->user->email }}
+                            <br> --}}
+                            {{ ucwords($_employee->first_name . ' ' . $_employee->last_name) }}
+                        </td>
                         <td>{{ strtoupper($_employee->first_name . ' ' . $_employee->last_name) }}</td>
                         <td>{{ strtoupper($_employee->department) }}</td>
-                        <td>
+                        {{-- <td>
                             @if ($_employee->daily_attendance_report)
                                 {{ date_format(date_create($_employee->daily_attendance_report->time_in), 'h:i:s a') }}
                             @else
@@ -41,7 +50,7 @@
                             @else
                                 -
                             @endif
-                        </td>
+                        </td> --}}
                     </tr>
                 @endforeach
 
