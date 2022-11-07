4b1dac9f11a55821251eadbfbdf71c645235d505
Percival Banting <percivalbanting@Percivals-MacBook-Air.local>
2022-11-07 11:43:56 +0800

    Executive Office Portal - Midshipman Absent List with Summary

--- resources/views/widgets/report/executive/onboarding-absent-report.blade.php
+++ resources/views/widgets/report/executive/onboarding-absent-report.blade.php
@@ -38,6 +38,9 @@
                 </tr>
             </thead>
         </table>
+        @php
+            $_section_count = [];
+        @endphp
         <table class="table-2">
             <thead>
                 <tr>
@@ -50,9 +53,67 @@
             <tbody>
                 /* Checking if there is a section. */
                 @if (count($_sections) > 0)
-                    @foreach ($_sections as $section)
+                    @foreach ($_sections as $key => $section)
+                        @php
+                            array_search($section->id, $_section_count) ?: array_push($_section_count, 0);
+                        @endphp
                         @if ($section->count() > 0)
                             @if (count($section->student_sections) > 0)
+                                @foreach ($section->student_sections as $item)
+                                    @if (!$item->student->onboarding_attendance)
+                                        @php
+                                            $_section_count[$key] += 1;
+                                        @endphp
+                                        <tr>
+                                            <td style="padding-left: 10px; width:20%">{{ $section->section_name }}</td>
+                                            <td style="padding-left: 10px; width:40%">
+                                                {{ strtoupper($item->student->last_name . ', ' . $item->student->first_name . ' ' . $item->student->middle_name) }}
+                                            </td>
+                                            <td class="text-center"style="padding-left: 10px; width:20%">
+                                                {{ $item->student->municipality . ', ' . $item->student->province }}
+                                            </td>
+                                            <td style="padding-left: 10px; width:30%">
+
+                                            </td>
+                                        </tr>
+                                    @else
+                                    @endif
+                                @endforeach
+                            @endif
+                        @endif
+                    @endforeach
+                @else
+                    <tr>
+                        <td colspan="3">No Section</td>
+                    </tr>
+                @endif
+            </tbody>
+        </table>
+        <h3 class="text-center"><b>SUMMARY OF MIDSHIPMAN ABSENT </b></h3>
+        {{-- {{ dd($_section_count) }} --}}
+        <table class="table-2">
+            <thead>
+                <tr>
+                    <th>SECTION NAME</th>
+                    <th>TOTAL ABSENT</th>
+                    <th>REMARKS</th>
+                    {{-- <th>ADDRESS</th>
+                    <th>REMARKS</th> --}}
+                </tr>
+            </thead>
+            <tbody>
+                /* Checking if there is a section. */
+                @if (count($_sections) > 0)
+                    @foreach ($_sections as $key => $section)
+                        @if ($section->count() > 0)
+                            <tr>
+                                <td style="padding-left: 10px; width:50%">{{ $section->section_name }}
+                                </td>
+                                <td class="text-center"style="padding-left: 10px; width:20%">{{ $_section_count[$key] }} /
+                                    {{ $section->student_sections->count() }}</td>
+                                <td></td>
+                            </tr>
+                            {{-- @if (count($section->student_sections) > 0)
                                 @foreach ($section->student_sections as $item)
                                     @if (!$item->student->onboarding_attendance)
                                         <tr>
@@ -69,7 +130,7 @@
                                         </tr>
                                     @endif
                                 @endforeach
-                            @endif
+                            @endif --}}
                         @endif
                     @endforeach
                 @else
@@ -79,6 +140,5 @@
                 @endif
             </tbody>
         </table>
-
     </div>
 @endsection
