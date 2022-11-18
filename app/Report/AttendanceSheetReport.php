d0ee27c53f3c0781c160c8d83b0679b4833149d8
Percival Banting <percivalbanting@Percivals-MacBook-Air.local>
2022-11-18 15:52:13 +0800

    Employee List with Picture

--- app/Report/AttendanceSheetReport.php
+++ app/Report/AttendanceSheetReport.php
@@ -11,6 +11,7 @@ class AttendanceSheetReport
     {
 
         $this->legal = [0, 0, 612.00, 1008.00];
+        $this->crosswise_short = [0, 0, 612, 738];
     }
     public function daily_report()
     {
@@ -25,7 +26,7 @@ class AttendanceSheetReport
             ->orderBy('staff.last_name', 'asc')->get();
         $pdf = PDF::loadView("widgets.report.employee.daily_report_attendance", compact('_employees'));
         $file_name = "Daily Attendance: "; // With Date now
-        return $pdf->setPaper($this->legal, 'portrait')->stream($file_name . '.pdf');
+        return $pdf->setPaper($this->crosswire_short, 'portrait')->stream($file_name . '.pdf');
     }
     public function daily_time_record_report($_start_date, $_end_date)
     {
