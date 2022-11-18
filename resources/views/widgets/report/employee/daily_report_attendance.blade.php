47b3104ea3b7b2cf3980ec731729240a4a6999de
Percival Banting <percivalbanting@Percivals-MacBook-Air.local>
2022-11-18 13:33:13 +0800

    Employee List of QRcode

--- resources/views/widgets/report/employee/daily_report_attendance.blade.php
+++ resources/views/widgets/report/employee/daily_report_attendance.blade.php
@@ -3,17 +3,19 @@
 @section('form-code', 'ACC-14 ')
 @section('content')
     <div class="content">
-        <h3 class="text-center"><b>DAILY ATTENDANCE REPORT</b></h3>
+        {{--  <h3 class="text-center"><b>DAILY ATTENDANCE REPORT</b></h3>
         <p>Date:
             <b><u>{{ request()->input('_date') ? date_format(date_create(request()->input('_date')), 'M d,Y') : now()->format('M d,Y') }}</u></b>
-        </p>
+        </p> --}}
 
         <table class="table-2">
             <thead>
                 <tr>
                     <th>QR CODE</th>
                     <th>NAME</th>
-                    <th>DEPARTMENT</th>
+                    <th></th>
+
+                    {{--  <th>DEPARTMENT</th> --}}
                     {{-- <th width="100">TIME-IN</th>
                     <th width="100">TIME-OUT</th> --}}
 
@@ -22,16 +24,44 @@
             <tbody>
                 @foreach ($_employees as $_employee)
                     <tr>
-                        <td class="text-center">
+                        <td class="text-center" style="width: 25%; hiegth:100px;">
+                            <br><br><br><br>
                             <img src="data:image/png;base64, {!! base64_encode(
-                                QrCode::style('round', 0.5)->eye('square')->size(200)->generate($_employee->user->email),
-                            ) !!} "> <br>
-                            {{--  {{ $_employee->user->email }}
-                            <br> --}}
-                            {{ ucwords($_employee->first_name . ' ' . $_employee->last_name) }}
+                                QrCode::style('round', 0.5)->eye('square')->size(170)->generate('employee:' . $_employee->user->email),
+                            ) !!} ">
+                            <br><br>
+                            <label for="" style="font-size: 14px">
+                                {{ ucwords($_employee->first_name . ' ' . $_employee->last_name) }}
+                            </label>
+                            <br>
+                            <label for="" style="font-size: 9px">
+                                {{ ucwords($_employee->department) }}
+                            </label>
+                            <br><br>
                         </td>
-                        <td>{{ strtoupper($_employee->first_name . ' ' . $_employee->last_name) }}</td>
-                        <td>{{ strtoupper($_employee->department) }}</td>
+                        <td style="width: 25%;text-align:center;">
+                            <img src="{{ public_path() . '/assets/image/bma-logo-1.png' }}" alt="" width="150">
+                            <br> bma.edu.ph
+                        </td>
+                        <td class="text-center" style="width: 30%; hiegth:100px;">
+                            {{--  <br><br>
+                            <img src="data:image/png;base64, {!! base64_encode(
+                                QrCode::style('round', 0.5)->eye('square')->size(170)->generate('employee:' . $_employee->user->email),
+                            ) !!} ">
+                            <br>
+                            <label for="">
+                                {{ ucwords($_employee->first_name . ' ' . $_employee->last_name) }}
+                            </label> --}}
+
+                        </td>
+                        {{--  <td style="width: 30%;text-align:center;">
+                            <img src="{{ public_path() . '/assets/image/bma-logo-1.png' }}" alt="" width="150">
+                            <br> bma.edu.ph
+                        </td> --}}
+                        {{-- <td>
+                            {{ strtoupper($_employee->first_name . ' ' . $_employee->last_name) }}
+                            <br>{{ strtoupper($_employee->department) }}
+                        </td> --}}
                         {{-- <td>
                             @if ($_employee->daily_attendance_report)
                                 {{ date_format(date_create($_employee->daily_attendance_report->time_in), 'h:i:s a') }}
