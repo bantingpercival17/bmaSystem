9610dcef3539bd135c14d1a248080373a1ad1f56
bantingpercival <percivalbanting@gmail.com>
2023-03-13 09:37:47 +0800

    Student Onboard Enrollment

--- resources/views/onboardtraining/student/view.blade.php
+++ resources/views/onboardtraining/student/view.blade.php
@@ -58,7 +58,7 @@
                                                 <div class="form-group col-md">
                                                     <label class="form-label-sm"><small>COMPANY NAME</small></label>
                                                     <input type="text" class="form-control" name="company_name"
-                                                        value="{{ old('company_name') ?: $_midshipman->shipboard_training->company_name }}">
+                                                        value="{{ (old('company_name') ?: $_midshipman->shipboard_training) ? $_midshipman->shipboard_training->company_name : '' }}">
                                                     @error('company_name')
                                                         <span class="invalid-feedback text-danger" role="alert">
                                                             <small> <b>{{ $message }}</b> </small>
