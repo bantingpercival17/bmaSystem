a3f6c41971d1e8698a53a476c0bc303c323196c1
bantingpercival <percivalbanting@gmail.com>
2023-03-22 10:28:48 +0800

    Onboard -Vessel Type

--- resources/views/onboardtraining/student/view.blade.php
+++ resources/views/onboardtraining/student/view.blade.php
@@ -255,7 +255,7 @@
                                             <div class="form-group col-md-5">
                                                 <label class="form-label-sm"><small>VESSEL TYPE</small></label>
                                                 @php
-                                                    $_select = ['CONTAINER VESSEL', 'GENERAL CARGO', 'TANKER'];
+                                                    $_select = ['CONTAINER VESSEL', 'GENERAL CARGO', 'TANKER','BULK CARIER'];
                                                 @endphp
                                                 <select name="_type_vessel" class="form-select"
                                                     value="{{ old('_type_vessel') }}">
