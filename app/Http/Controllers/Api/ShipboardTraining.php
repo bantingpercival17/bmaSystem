f74df4190a38177c41c4cdff90ca7616c480203f
bantingpercival <percivalbanting@gmail.com>
2023-04-19 09:39:35 +0800

    SBT API

--- app/Http/Controllers/Api/ShipboardTraining.php
+++ app/Http/Controllers/Api/ShipboardTraining.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\AcademicYear;
 use App\Models\DeploymentAssesment;
 use App\Models\DocumentRequirements;
 use App\Models\Documents;
@@ -18,7 +19,7 @@ class ShipboardTraining extends Controller
     public function shipboard_performance_view(Request $_request)
     {
         try {
-            $shipboard_information = ShipBoardInformation::where('student_id', auth()->user()->student_id)->where('is_approved', true)->with('performance_report')->get();
+            $shipboard_information = ShipBoardInformation::where('student_id', auth()->user()->student_id)->with('document_requirements')->with('performance_report')->get();
             return response(['data' => compact('shipboard_information')], 200);
         } catch (Exception $error) {
             return response(['error' => $error->getMessage()], 505);
@@ -85,7 +86,7 @@ class ShipboardTraining extends Controller
             $_find = DeploymentAssesment::where('student_id', auth()->user()->student_id)
                 ->where('is_removed', false)
                 ->first();
-
+            $academic = AcademicYear::where('is_active', true)->first();
             $shipboard_information = ShipBoardInformation::where('student_id', auth()->user()->student_id)->first();
             if (!$shipboard_information) {
                 $shipboard_information =  ShipBoardInformation::create([
@@ -95,7 +96,7 @@ class ShipboardTraining extends Controller
                     'vessel_type' => $_request->vessel_type,
                     'shipping_company' => $_request->sea_experience,
                     'shipboard_status' => 'ON-GOING',
-                    'sbt_batch' => 'SBT',
+                    'sbt_batch' => 'SBT ' . $academic->school_year . " " . $academic->semester,
                     'embarked' => $_request->embarked,
                     'disembarked' => null
                 ]);
