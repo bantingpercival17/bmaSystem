6eebb6cd646d205aff11a342b673a555e4511fe8
bantingpercival17 <percivalbanting@gmail.com>
2023-08-03 14:45:17 +0800

    Enrollment Assesment

--- app/Models/Staff.php
+++ app/Models/Staff.php
@@ -183,7 +183,7 @@ class Staff extends Model
                 'role_id' => 3,
                 'role_name' => 'Registrar',
                 'role_icon' => 'icon-job',
-                'role_routes' => [['Dashboard', 'registrar.dashboard'], ['Enrollment', 'registrar.enrollment'], ['Enrollment v2', 'enrollment.view-v2'], ['Semestral Grades', 'registrar.semestral-grades'], ['Students', 'registrar.students'], ['Section', 'registrar.section-view'], ['Subjects', 'registrar.subject-view'], ['Semestral Clearance', 'registrar.semestral-clearance'],['Scholarship Grants','registrar.scholarship-grants']],
+                'role_routes' => [['Dashboard', 'registrar.dashboard'], /* ['Enrollment', 'registrar.enrollment'], */ ['Enrollment', 'enrollment.view-v2'], ['Semestral Grades', 'registrar.semestral-grades'], ['Students', 'registrar.students'], ['Section', 'registrar.section-view'], ['Subjects', 'registrar.subject-view'], ['Semestral Clearance', 'registrar.semestral-clearance'],['Scholarship Grants','registrar.scholarship-grants']],
             ],
             [
                 
