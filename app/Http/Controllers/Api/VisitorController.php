543ac04c082070e24ae5882d1b01df15598d3e8d
bantingpercival17 <percivalbanting@gmail.com>
2023-12-07 10:10:37 +0800

    Visitor Logs

--- app/Http/Controllers/Api/VisitorController.php
+++ app/Http/Controllers/Api/VisitorController.php
@@ -7,5 +7,19 @@ use Illuminate\Http\Request;
 
 class VisitorController extends Controller
 {
-    //
+    function visitor_logs(Request $request)
+    {
+        $userAgent = $request->header('User-Agent');
+        $ipAddress = $request->ip();
+        $jsonData = json_encode($userAgent);
+
+        // Generate a unique file name
+        $fileName = 'data_' . $ipAddress . '.json';
+
+        // Specify the file path
+        $filePath =  '/device/' . $fileName;
+        // Return the device information as a response or use it as needed
+        //return response()->json($deviceInfo);
+        return response()->json($ipAddress);
+    }
 }
