6733709c02e805985e696785c8062f64d0593c97
bantingpercival17 <percivalbanting@gmail.com>
2024-01-03 16:06:31 +0800

    Applicant Module - Document Verification, Update Examination Count and Message Alerts

--- app/Http/Controllers/Api/ApplicantController.php
+++ app/Http/Controllers/Api/ApplicantController.php
@@ -189,12 +189,12 @@ class ApplicantController extends Controller
         if ($account->applicant) {
             # If Account have Applicant Details it will be Update the information
             ApplicantDetials::where('applicant_id', $user->id)->update($_data);
-            return response(['message' => 'Applicant Information Completed'], 200);
+            return response(['message' => "Applicant's Information Completed"], 200);
         } else {
             # Then is not they will be Store the Applicant Details
             $_data['applicant_id'] = $account->id;
             ApplicantDetials::create($_data);
-            return response(['message' => 'Applicant Information Completed'], 200);
+            return response(['message' => "Applicant's Information Completed"], 200);
         }
     }
     function applicant_registration_form()
