31ae968443bee681ce20d7b85b0b1ee0c8199b9a
bantingpercival <percivalbanting@gmail.com>
2024-04-18 13:55:11 +0800

    Examination

--- app/Http/Controllers/Api/ExaminationController.php
+++ app/Http/Controllers/Api/ExaminationController.php
@@ -15,7 +15,7 @@ class ExaminationController extends Controller
     function review_examination()
     {
         try {
-            $examination = Examination::where('examination_name', 'like', '%ONBOARD EXAMINATION%')->with('category_lists')->first();
+            $examination = Examination::where('examination_name',  'ONBOARD EXAMINATION BSMT')->with('category_lists')->first();
             return response(compact('examination'), 200);
         } catch (\Throwable $th) {
             //throw $th;
