503532c726ae3dcab674bfc0bf1caf135bf90d2e
bantingpercival <percivalbanting@gmail.com>
2024-04-19 11:22:26 +0800

    Examination Mode

--- app/Http/Controllers/Api/ExaminationController.php
+++ app/Http/Controllers/Api/ExaminationController.php
@@ -15,7 +15,7 @@ class ExaminationController extends Controller
     function review_examination()
     {
         try {
-            $examination = Examination::where('examination_name',  'ENTRANCE EXAMINATION')->where('department','college')->with('category_lists')->first();
+            $examination = Examination::where('examination_name',  'REVIEW EXAMINATION')->where('department','college')->with('category_lists')->first();
             return response(compact('examination'), 200);
         } catch (\Throwable $th) {
             //throw $th;
